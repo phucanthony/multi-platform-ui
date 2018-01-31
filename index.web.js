532e0b25ab97cc7a32689615cf1df4aa56fc96ab
cloudle <lehaoson@gmail.com>
2018-01-31 19:01:53 +0700

    Update hack message...

--- index.web.js
+++ index.web.js
@@ -37,6 +37,6 @@ if (module.hot) {
 }
 
 if (process.env.NODE_ENV === 'production') {
-	console.log("%cHey, we have a challenge!", 'font-size:14px;color:#fff;text-shadow:0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);');
-	console.log("%cA hidden secret is waiting for you, however.. only genius could find and solve it ¯\\_(ツ)_/¯", 'font-size:15px;');
+	console.log("%cHey, we have a challenge!!!", 'font-size:14px;color:#fff;text-shadow:0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);');
+	console.log("%cA hidden secret is waiting for you! however, only genius could find and solve it ¯\\_(ツ)_/¯..", 'font-size:15px;');
 }
